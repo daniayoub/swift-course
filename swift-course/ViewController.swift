b01fb16dfaa89f57a6df1445bb0ae0953893214f
dan <leticiaatieh@Leticias-MacBook-Pro.local>
2021-08-26 19:34:01 +0300

    first commit

---
+++ swift-course/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-course
+//
+//  Created by dan on 8/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
